cd52313856cadee316fc2c366d56d7679957c2e1
Vistonho <vistonho@gmail.com>
2023-11-06 09:28:10 +0300

    test commit 6

--- views/site/about.php
+++ views/site/about.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content!
+        Эта странца о нас переведа на русский язык благодаря трудолюбивым разработчикам!
     </p>
 
     <code><?= __FILE__ ?></code>
