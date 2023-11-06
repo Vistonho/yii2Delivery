3574373b5d33154951fd6370f88593541c3092a8
Vistonho <Vistonho@gmail.com>
2023-11-06 15:42:01 +0300

    add list of attribute current user in profile page

--- views/site/profile.php
+++ views/site/profile.php
@@ -7,6 +7,7 @@
 use yii\bootstrap5\ActiveForm;
 use yii\bootstrap5\Html;
 use yii\captcha\Captcha;
+use yii\helpers\VarDumper;
 
 $this->title = 'Личный кабинет';
 $this->params['breadcrumbs'][] = $this->title;
@@ -20,12 +21,27 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php $form = ActiveForm::begin(['id' => 'photo-form', 'options' => ['enctype' => 'multipart/form-data']]); ?>
 
-    <?= $form->field($model, 'photo')->fileInput() ?>
+
 
     <div class="form-group">
+        <?= $form->field($model, 'photo')->fileInput() ?>
         <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary', 'name' => 'photo-button']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
+
+    <?php
+        $identity = Yii::$app->user->identity;
+        // VarDumper::dump($identity->attributes, 10, true);
+    ?>
+
+    <?= "<h5>Имя: $identity->name</h5>" ?>
+    <?= "<h5>Фамилия: $identity->surname</h5>" ?>
+    <?= "<h5>Отчество: $identity->patronymic</h5>" ?>
+    <?= "<h5>Логин: $identity->login</h5>" ?>
+    <?= "<h5>Почта: $identity->email</h5>" ?>
+    <?= "<h5>Телефон: $identity->phone</h5>" ?>
+
+
 
 </div>
