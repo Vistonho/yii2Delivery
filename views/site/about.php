6cb57a8134405b307fa1b8951447dc160e9d0cfa
Vistonho <Vistonho@gmail.com>
2023-11-06 09:29:01 +0300

    test commit 7

--- views/site/about.php
+++ views/site/about.php
@@ -10,9 +10,14 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <h2>О нас второй раз</h2>
+
     <p>
-        This is the About page. You may modify the following file to customize its content!
+        This is the About page. You may modify the following file to customize its content!</br>
+        Hello there!
     </p>
+
+
 
     <code><?= __FILE__ ?></code>
 </div>
